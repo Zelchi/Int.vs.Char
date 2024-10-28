fce8825202317a6686a739011c4dc3f4cfc3d5d9
Zelchi <tininhakawaii@gmail.com>
2024-10-28 14:18:45 -0300

    "Varias mudanças"

--- main.c
+++ main.c
@@ -2,57 +2,122 @@
 #include <locale.h>
 #include <conio.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define y 10
 #define x 10
 
 int posicao = 5;
-
+void menu(void);
 void jogo(void);
+void comandos(int *vivo);
+int sair();
+int sairMenu = 1;
 void exibirMapa(char mapa[y][x]);
+void atualizarMapa(char mapa[y][x]);
 
 int main(void)
 {
+    setlocale(LC_ALL, "UTF-8");
+
     do
     {
-        jogo();
-    } while (1);
+        menu();
+    } while (sairMenu);
 
     return 0;
 }
+
 void jogo(void)
 {
-    char mapa[y][x] = {
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-    };
-    char tecla;
-     // Exibe o mapa na tela
-    mapa[8][posicao] = '1';
-    exibirMapa(mapa);
+    int vivo = 1;
 
-    scanf("%c", &tecla);
-    if (tecla == 'A' || tecla == 'a') {
+    do
+    {
+        system("cls");
+        char mapa[y][x] = {
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}};
+        mapa[8][posicao] = '1';
+        
+        atualizarMapa(mapa);
+        comandos(&vivo);
+    } while (vivo);
+}
+
+void atualizarMapa(char mapa[y][x])
+{
+    struct timespec data;
+    timespec_get(&data, TIME_UTC);
+    long long tempo = data.tv_sec;
+    printf("%lld\n", tempo);
+
+    exibirMapa(mapa);
+}
+
+void comandos(int *vivo)
+{
+    char tecla = getch();
+
+    fflush(stdin);
+    if (tecla == 'A' || tecla == 'a')
+    {
         posicao--;
     }
-    if (tecla == 'D' || tecla == 'd') {
+    if (tecla == 'D' || tecla == 'd')
+    {
         posicao++;
     }
+    if (posicao == 0)
+    {
+        posicao++;
+    }
+    if (posicao == x - 1)
+    {
+        posicao--;
+    }
+    if (tecla == 27)
+    {
+        *vivo = 0;
+    }
 }
+
 void exibirMapa(char mapa[y][x])
 {
-    for (int i = 0; i < y; i++) {
-        for (int j = 0; j < x; j++) {
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
             printf("%c ", mapa[i][j]);
         }
         printf("\n");
     }
+}
+
+void menu(void)
+{
+    system("cls");
+    printf("1 - Jogo\n2 - Placar\n3 - Sair\n");
+    switch (getch())
+    {
+    case '1':
+        jogo();
+        break;
+    case '3':
+        sair();
+        break;
+    }
+}
+
+int sair()
+{
+    sairMenu = 0;
 }
